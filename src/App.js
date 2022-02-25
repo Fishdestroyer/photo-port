822eb3dde0e42cff238e3faaa7cf70e908fb9a69
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-25 01:32:56 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory ={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
